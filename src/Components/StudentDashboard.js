73c66f89ba307ae6f8e4d761d78aff89c4026d27
kunalsupekar <kunal.supekar@mitaoe.ac.in>
2023-10-29 23:28:27 +0530

    third commit

--- src/Components/StudentDashboard.js
+++ src/Components/StudentDashboard.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './StudentDashboard.css';
-import StudentNavbar from './StudentNavbar';
+import StudentNavbar from './DashboardComponents/StudentNavbar';
+import StudentDashboardCards from './DashboardComponents/StudentDashboardCards';
+import Sidebar from './DashboardComponents/Sidebar';
+
+
 function StudentDashboard() {
   return (
-    <>
+    <div>
+      <nav>
+
     <StudentNavbar/>
-    </>
+      </nav>
+  
+    <main className='sidebar-main'>
+      <div className="side-sidebar">
+   <Sidebar/>
+      </div>
+      <div className="sidebar-cards">
+
+    <StudentDashboardCards/>
+      </div>
+
+    </main>
+   {/* <StudentDashboardCards/> */}
+    </div>
   );
 }
 
