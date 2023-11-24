cf88fcd5cd669ffc8f82bd31dc5eef5842335e46
kunalsupekar <kunal.supekar@mitaoe.ac.in>
2023-11-24 22:19:02 +0530

    footers

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -4,7 +4,7 @@ export default function
 () {
   return (
     <>
-<img src="https://media.istockphoto.com/id/1456749194/photo/college-students-arriving-for-night-school.jpg?s=1024x1024&w=is&k=20&c=PiS4Hn3S366cV-CeszZ7IyTIbMg3aebclyIVpK32fFg="alt="Description of the image" />
+<img src="https://media.istockphoto.com/id/1456749194/photo/college-students-arriving-for-night-school.jpg?s=1024x1024&w=is&k=20&c=PiS4Hn3S366cV-CeszZ7IyTIbMg3aebclyIVpK32fFg="alt="Description of collge" />
     </>
   )
 }
